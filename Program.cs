3aa2d8210b83188e1252424c5778603ad5180f0f
Casper Koksby Møller Nielsen <casperkmn@gmail.com>
2021-08-10 09:18:18 +0200

    Added jwt token

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SoSicencneSSHAgent.jwt;
+using SoSicencneSSHAgent.jwt.managers;
+using SoSicencneSSHAgent.jwt.models;
 using SoSicencneSSHAgent.MicroServices;
 using SoSicencneSSHAgent.SSHClasss;
 
@@ -11,15 +17,20 @@ namespace SoSicencneSSHAgent
     {
         static void Main(string[] args)
         {
-            SSHAgent agent = new SSHAgent();
-            Thread th = new Thread(agent.CreateSshTunnel);
-            th.Start();
-            CreateHostBuilder(args).Build().StartAsync();
-            Console.WriteLine("The BackGroundservice have been started, press enter to stop this program.");
+            string token = new JWTController().CreateToken("hello world", RoleType.theacher);
+            Console.WriteLine("token: " + token);
+            Console.WriteLine("Validate: " + new JWTController().ValidateJWT(token));
+            Console.WriteLine("Validate For theacher: " + new JWTController().ValidateRoleLevel(token, RoleType.theacher));
+            Console.WriteLine("username: " + new JWTController().GetUsername(token));
+
             Console.ReadLine();
 
-
-
+            //SSHAgent agent = new SSHAgent();
+            //Thread th = new Thread(agent.CreateSshTunnel);
+            //th.Start();
+            //CreateHostBuilder(args).Build().StartAsync();
+            //Console.WriteLine("The BackGroundservice have been started, press enter to stop this program.");
+            //Console.ReadLine();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ jwt/managers/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using SoSicencneSSHAgent.jwt.models;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace SoSicencneSSHAgent.jwt.managers
+{
+    interface IAuthService
+    {
+        string SecretKey { get; set; }
+
+        bool IsTokenValid(string token);
+        string GenerateToken(IAuthContainerModel model);
+        IEnumerable<Claim> GetTokenClaims(string token);
+    }
+}
